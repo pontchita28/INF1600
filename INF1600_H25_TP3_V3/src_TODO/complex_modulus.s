1d6f3e73f9ab08ae7647f96540a7be6446f74a2c
Soeuchelle Michel <somic@l3818-15.info.polymtl.ca>
2025-03-14 08:46:08 -0400

    ajout des parties pour le calcul du modulus des nombres complexes. besoin de corriger les parties pour faire les calculs avec les bons opérands

--- INF1600_H25_TP3_V3/src_TODO/complex_modulus.s
+++ INF1600_H25_TP3_V3/src_TODO/complex_modulus.s
@@ -23,6 +23,49 @@ _ZNK7Complex7modulusEv:
 
     # TODO
 
+    // pour avoir le modulus: on doit faire sqrt(image^2 + reel^2)
+
+
+    // faddp: fait la somme de la source et de la destination et mets le résultat
+    // dans la destination 
+    
+    // on a deux fonctions qui nous permettent de retourner 
+    // la partie image et la partie réelle directement 
+    // faire des appels de fonctions pour pouvoir les utiliser 
+    // en faisant un push premièrement pour leurs paramètres respectifs
+    // pour mettre leur utilisation 
+
+    // pas de paramètres, donc appel direct des fonctions
+
+    // appel de la fonction pour avoir la valeur réelle 
+    call _ZNK7Complex8realPartEv # va mettre le résultat dans le premier registre de la FPU ST[0]
+
+    // calcul de la partie réelle au carré (se trouve dans ST[1])
+    // chargement de cette même valeur au dessus de la pile 
+    flds
+    fmulp %ST(1), %ST(1) # multiplication de la valeur qui se trouve dans cette adresse par elle-même
+    
+    
+    
+    
+    
+    // appel de la fonction pour avoir la partie complexe 
+    call _ZNK7Complex8imagPartEv # va mettre le résultat dans le premier registre de la FPU ST[0]
+        // décalage de la valeur orginale de la valeur réelle dans ST[1]
+
+    
+    
+    // calcul de la partie complexe au carré (se trouve exactement à esp)
+    fmulp %st(0), %st(0) # multiplication de la valeur qui se touve dans cette adresse par elle-même
+
+    // addition des deux valeurs pour la mettre au top de la FPU, donc ST(0)
+    faddp %st(1), %st(0) # addition de ST(1) + ST(0) pour la mettre dans ST(0)
+
+    // faire une racine carrée du résultat 
+    fsqrt %st(0), %st(0) # retourner la valeur de la racine carrée dans ST(0)
+
+    // comme on veut retourner un flottant, on laisse la valeur dans ST(0)
+
     # epilogue
     leave
     ret
